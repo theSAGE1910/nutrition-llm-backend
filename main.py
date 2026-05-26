bc8b292d1cf22a71cc841cb20c04c30e2f20d230
theSAGE1910 <108081452+theSAGE1910@users.noreply.github.com>
2026-05-26 17:35:15 +0200

    Add frontend and CORS middleware

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 from routers import meals, coach
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(meals.router)
 app.include_router(coach.router)
